a09aa455a19cecd3adc7377d448c74248729feb9
Aedan Smith <aedan.smith@gmail.com>
2018-10-06 15:53:01 -0700

    Use (lambda x y ...) and (lambda ...) syntax

--- m1/src/mc.m
+++ m1/src/mc.m
@@ -31,9 +31,8 @@
 (def generate)
 
 (def field
-  (lambda name
-    (lambda data
-      (dot data name))))
+  (lambda name data
+    (dot data name)))
 
 (def pair
   (data-constructor (keyword pair)
@@ -42,53 +41,33 @@
       nil))))
 
 (def new-pair
-  (lambda first
-    (lambda second
-      (pair (cons first (cons second nil))))))
+  (lambda first second
+    (pair (cons first (cons second nil)))))
 
 (def first (field (keyword first)))
 (def second (field (keyword second)))
 
 (def and
-  (lambda x
-    (lambda y
-      (if x (y nil) false))))
+  (lambda x y
+    (if x (y nil) false)))
 
 (def or
-  (lambda x
-    (lambda y
-      (if x true (y nil)))))
+  (lambda x y
+    (if x true (y nil))))
 
 (def not
   (lambda x
     (if x false true)))
 
 (def compose
-  (lambda f
-    (lambda g
-      (lambda x
-        (f (g x))))))
+  (lambda f g x
+    (f (g x))))
 
 (def is-nil
   (lambda x
     (eq-keyword (type-name x) (keyword nil))))
 
 (def cadr (compose car cdr))
-
-(def fold-left
-  (lambda list
-    (lambda init
-      (lambda f
-        (if (is-nil list)
-          init
-          (fold-left (cdr list) (f init (car list)) f))))))
-
-(def map
-  (lambda list
-    (lambda f
-      (if (is-nil list)
-        nil
-        (cons (f (car list)) (cdr list))))))
 
 (def one (i (keyword 1)))
 
@@ -103,15 +82,15 @@
 (def is-newline
   (lambda char
     (or (eq-char char newline)
-        (lambda unused
+        (lambda
           (eq-char char carriage-return)))))
 
 (def is-whitespace
   (lambda char
     (or (is-newline char)
-        (lambda unused
+        (lambda
           (or (eq-char char space)
-              (lambda unused
+              (lambda
                 (eq-char char tab)))))))
 
 (def file.read (field (keyword read)))
@@ -135,10 +114,8 @@
       nil)))))
 
 (def new-parse-success
-  (lambda result
-    (lambda state
-      (lambda rest
-        (parse-success (cons result (cons state (cons rest nil))))))))
+  (lambda result state rest
+    (parse-success (cons result (cons state (cons rest nil))))))
 
 (def parse-success.result (field (keyword result)))
 (def parse-success.state (field (keyword state)))
@@ -149,112 +126,94 @@
     (eq-keyword (type-name x) (keyword parse-success))))
 
 (def predicate-parser
-  (lambda f
-    (lambda input
-      (lambda state
-        ((lambda input
-          (if (and
-                (not (is-nil input))
-                (lambda unused (f (car input))))
-            (new-parse-success (car input) state (cdr input))
-            (new-parse-failure state)))
-        input)))))
+  (lambda f input state
+    ((lambda input
+      (if (and (not (is-nil input))
+               (lambda (f (car input))))
+        (new-parse-success (car input) state (cdr input))
+        (new-parse-failure state)))
+    input)))
 
 (def map-parser
-  (lambda parser
-    (lambda f
-      (lambda input
-        (lambda state
-          (f (parser input state)))))))
+  (lambda parser f input state
+    (f (parser input state))))
 
 (def map-parser-success
-  (lambda parser
-    (lambda f
-      (map-parser parser
-        (lambda result
-          (if (is-parse-success result)
-            (f result)
-            result))))))
+  (lambda parser f
+    (map-parser parser
+      (lambda result
+        (if (is-parse-success result)
+          (f result)
+          result)))))
 
 (def map-parser-result
-  (lambda parser
-    (lambda f
-      (map-parser-success parser
-        (lambda success
-          (new-parse-success
-            (f (parse-success.result success))
-            (parse-success.state success)
-            (parse-success.rest success)))))))
+  (lambda parser f
+    (map-parser-success parser
+      (lambda success
+        (new-parse-success
+          (f (parse-success.result success))
+          (parse-success.state success)
+          (parse-success.rest success))))))
 
 (def map-parser-state
-  (lambda parser
-    (lambda f
-      (map-parser-success parser
-        (lambda success
-          (new-parse-success
-            (parse-success.result success)
-            (f (parse-success.state success))
-            (parse-success.rest success)))))))
+  (lambda parser f
+    (map-parser-success parser
+      (lambda success
+        (new-parse-success
+          (parse-success.result success)
+          (f (parse-success.state success))
+          (parse-success.rest success))))))
 
 (def inject-past-state
-  (lambda parser
-    (lambda input
-      (lambda state
-        ((map-parser-result parser
-          (lambda result
-            (new-pair result state)))
-        input state)))))
+  (lambda parser input state
+    ((map-parser-result parser
+      (lambda result
+      (new-pair result state)))
+    input state)))
 
 (def combine-parser
-  (lambda parser1
-    (lambda parser2
-      (lambda input
-        (lambda state
-          ((lambda parser1-result
-            (if (is-parse-success parser1-result)
-              ((lambda parser2-result
-                (if (is-parse-success parser2-result)
-                  (new-parse-success
-                    (new-pair
-                      (parse-success.result parser1-result)
-                      (parse-success.result parser2-result))
-                    (parse-success.state parser2-result)
-                    (parse-success.rest parser2-result))
-                  parser2-result))
-              (parser2
-                (parse-success.rest parser1-result)
-                (parse-success.state parser1-result)))
-              parser1-result))
-          (parser1 input state)))))))
+  (lambda parser1 parser2 input state
+    ((lambda parser1-result
+      (if (is-parse-success parser1-result)
+        ((lambda parser2-result
+          (if (is-parse-success parser2-result)
+            (new-parse-success
+              (new-pair
+                (parse-success.result parser1-result)
+                (parse-success.result parser2-result))
+              (parse-success.state parser2-result)
+              (parse-success.rest parser2-result))
+            parser2-result))
+        (parser2
+          (parse-success.rest parser1-result)
+          (parse-success.state parser1-result)))
+        parser1-result))
+    (parser1 input state))))
 
 (def combine-parser-left
-  (lambda parser1
-    (lambda parser2
-      (map-parser-result (combine-parser parser1 parser2) first))))
+  (lambda parser1 parser2
+    (map-parser-result (combine-parser parser1 parser2) first)))
 
 (def combine-parser-right
-  (lambda parser1
-    (lambda parser2
-      (map-parser-result (combine-parser parser1 parser2) second))))
+  (lambda parser1 parser2
+    (map-parser-result (combine-parser parser1 parser2) second)))
 
 (def repeat-parser
-  (lambda parser
-    (lambda input
-      (lambda state
-        ((lambda result
-          (if (is-parse-success result)
-            ((lambda rest-result
-              (new-parse-success
-                (cons
-                  (parse-success.result result)
-                  (parse-success.result rest-result))
-                (parse-success.state rest-result)
-                (parse-success.rest rest-result)))
-            (repeat-parser parser
-              (parse-success.rest result)
-              (parse-success.state result)))
-            (new-parse-success nil state input)))
-        (parser input state))))))
+  (lambda parser input state
+    ((lambda result
+      (if (is-parse-success result)
+        ((lambda rest-result
+          (new-parse-success
+            (cons
+              (parse-success.result result)
+              (parse-success.result rest-result))
+            (parse-success.state rest-result)
+            (parse-success.rest rest-result)))
+        (repeat-parser parser
+          (parse-success.rest result)
+          (parse-success.state result)))
+        (new-parse-success nil state input)))
+    (parser input state))))
 
 (def repeat-parser1
   (lambda parser
@@ -264,21 +223,16 @@
           (cons (first pair) (second pair))))))
 
 (def alternative-parser
-  (lambda parser1
-    (lambda parser2
-      (lambda input
-        (lambda state
-          ((lambda parser1-result
-            (if (is-parse-success parser1-result)
-              parser1-result
-              (parser2 input state)))
-          (parser1 input state)))))))
+  (lambda parser1 parser2 input state
+    ((lambda parser1-result
+      (if (is-parse-success parser1-result)
+        parser1-result
+        (parser2 input state)))
+    (parser1 input state))))
 
 (def lazy-parser
-  (lambda parser
-    (lambda input
-      (lambda state
-        ((parser nil) input state)))))
+  (lambda parser input state
+    ((parser nil) input state)))
 
 (def identifier-expr
   (data-constructor (keyword identifier-expr)
@@ -287,12 +241,15 @@
       nil))))
 
 (def new-identifier-expr
-  (lambda name
-    (lambda line
-      (identifier-expr (cons name (cons line nil))))))
+  (lambda name line
+    (identifier-expr (cons name (cons line nil)))))
 
 (def identifier-expr.name (field (keyword name)))
 (def identifier-expr.line (field (keyword line)))
+
+(def is-identifier-expr
+  (lambda expr
+    (eq-keyword (type-name expr) (keyword identifier-expr))))
 
 (def list-expr
   (data-constructor (keyword list-expr)
@@ -301,12 +258,15 @@
       nil))))
 
 (def new-list-expr
-  (lambda exprs
-    (lambda line
-      (list-expr (cons exprs (cons line nil))))))
+  (lambda exprs line
+    (list-expr (cons exprs (cons line nil)))))
 
 (def list-expr.exprs (field (keyword exprs)))
 (def list-expr.line (field (keyword line)))
+
+(def is-list-expr
+  (lambda expr
+    (eq-keyword (type-name expr) (keyword list-expr))))
 
 (def newline-parser
   (map-parser-state
@@ -326,9 +286,9 @@
   (lambda char
     (not
       (or (is-whitespace char)
-          (lambda unused
+          (lambda
             (or (eq-char char open-parentheses)
-                (lambda unused
+                (lambda
                   (eq-char char close-parentheses))))))))
 
 (def parser)
@@ -359,7 +319,7 @@
         (combine-parser-right
           open-paren-parser
           (combine-parser-left
-            (lazy-parser (lambda unused parser))
+            (lazy-parser (lambda parser))
             close-paren-parser)))
       (lambda pair
         (new-list-expr (first pair) (second pair))))))
@@ -378,12 +338,11 @@
       (parser input one))))
 
 (def compile
-  (lambda in-file
-    (lambda out-file
-      (run-with (file.read in-file)
-        (lambda char-stream
-          (generate in-file out-file
-            (parse char-stream)))))))
+  (lambda in-file out-file
+    (run-with (file.read in-file)
+      (lambda char-stream
+        (generate in-file out-file
+          (parse char-stream))))))
 
 (run-unsafe
   (compile
