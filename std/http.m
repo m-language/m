e684c44ed0886589fac2612234c53f48b0cfc279
Kaden Thomas <thomas.kaden4@outlook.com>
2019-05-21 11:30:26 -0700

    Add rudimentary HTTP support

---
+++ std/http.m
@@ -0,0 +1,22 @@
+;;; http.m
+;;;
+;;; An HTTP client for M (temporary)
+
+;; Send a request synchronously, returning a process
+(extern http/send)
+
+;; Supported HTTP methods
+(def http/GET   (symbol GET))
+(def http/HEAD  (symbol HEAD))
+(def http/POST  (symbol POST))
+(def http/PUT   (symbol PUT))
+(def http/PATCH (symbol PATCH))
+(def http/DELETE (symbol DELETE))
+
+;; Convenience methods for each HTTP method
+(defn http/get url headers body    (http/send http/GET url headers body))
+(defn http/head url headers body   (http/send http/HEAD url headers body))
+(defn http/post url headers body   (http/send http/POST url headers body))
+(defn http/put url headers body    (http/send http/PUT url headers body))
+(defn http/patch url headers body  (http/send http/PATCH url headers body))
+(defn http/delete url headers body  (http/send http/DELETE url headers body))
