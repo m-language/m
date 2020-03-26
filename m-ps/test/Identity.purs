298ee0a00562c88ab615e817bb84f0f8956e6291
aedans <aedan.smith@outlook.com>
2020-03-25 18:09:05 -0700

    Fix unit tests

--- m-ps/test/Identity.purs
+++ m-ps/test/Identity.purs
@@ -2,32 +2,31 @@ module Test.Identity where
 
 import Prelude
 
-import Data.Foldable (fold, foldl)
-import Data.List (List(..), (:), fromFoldable, singleton)
+import Data.Foldable (fold)
+import Data.List (List(..))
 import Data.Newtype (class Newtype)
 import Data.NonEmpty ((:|))
 import Test.QuickCheck (class Arbitrary)
 import Test.QuickCheck.Gen (Gen, arrayOf, oneOf, resize)
 import Tree (Tree(..))
 
-applyIdentity :: Tree -> Tree -> Tree
-applyIdentity f a = ApplyTree (f : a : Nil)
-
-curryIdentity :: Tree -> Tree
-curryIdentity (ApplyTree (car : cdr)) = foldl (\f x -> ApplyTree (f : x : Nil)) (curryIdentity car) cdr
-curryIdentity tree = ApplyTree $ singleton tree
-
 fnIdentity :: Tree -> Tree
-fnIdentity tree = ApplyTree $ fromFoldable [SymbolTree "fn", SymbolTree "x", SymbolTree "x", tree]
+fnIdentity tree = ApplyTree (ApplyTree (ApplyTree (SymbolTree "fn") (SymbolTree "x")) (SymbolTree "x")) tree
 
 fmIdentity :: Tree -> Tree
-fmIdentity tree = ApplyTree $ fromFoldable [SymbolTree "fm", SymbolTree "x", SymbolTree "x", tree]
+fmIdentity tree = ApplyTree (ApplyTree (ApplyTree (SymbolTree "fm") (SymbolTree "x")) (SymbolTree "x")) tree
+
+fn0Identity :: Tree -> Tree
+fn0Identity tree = ApplyTree (ApplyTree (SymbolTree "fn") (ListTree Nil)) tree
+
+fm0Identity :: Tree -> Tree
+fm0Identity tree = ApplyTree (ApplyTree (SymbolTree "fm") (ListTree Nil)) tree
 
 blockIdentity :: Tree -> Tree
-blockIdentity tree = ApplyTree $ fromFoldable [SymbolTree "block", ApplyTree Nil, tree]
+blockIdentity tree = ApplyTree (ApplyTree (SymbolTree "block") (ListTree Nil)) tree
 
 defIdentity :: Tree -> Tree
-defIdentity tree = ApplyTree $ fromFoldable [SymbolTree "def", SymbolTree "x", tree, SymbolTree "x"]
+defIdentity tree = ApplyTree (ApplyTree (ApplyTree (SymbolTree "def") (SymbolTree "x")) tree) (SymbolTree "x")
 
 newtype Identity = Identity (Tree -> Tree)
 
@@ -44,9 +43,10 @@ identityGen = oneOf $ (pure $ Identity \x -> x) :|
     map (pure <<< Identity) 
       [ fnIdentity
       , fmIdentity
+      , fn0Identity
+      , fm0Identity
       , blockIdentity
-      , defIdentity
-      , curryIdentity ]
+      , defIdentity ]
 
 instance arbitraryIdentity :: Arbitrary Identity where
   arbitrary = resize 100 $ fold <$> arrayOf identityGen
