f785e32f6f948db58c9003085fec12a27199db29
aedans <aedan.smith@outlook.com>
2020-03-25 19:56:44 -0700

    Fix tests

--- m-ps/test/Main.purs
+++ m-ps/test/Main.purs
@@ -2,13 +2,10 @@ module Test.Main where
 
 import Prelude
 
-import Control.Monad.Except (runExceptT)
-import Control.Monad.Reader (runReaderT)
-import Control.Monad.Trampoline (runTrampoline)
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Eval (eval)
-import Eval.Types (asString)
+import Eval.Types (asString, runEvalResult)
 import Partial.Unsafe (unsafePartial)
 import Special (special)
 import Test.Identity (Identity(..))
@@ -22,6 +19,6 @@ testIdentity :: Partial => Identity -> Result
 testIdentity (Identity identity) = 
   let tree = StringTree "Hello"
       identicalTree = identity tree
-      result = runTrampoline $ runExceptT $ runReaderT (asString $ eval $ Tuple mempty tree) special
-      identicalResult = runTrampoline $ runExceptT $ runReaderT (asString $ eval $ Tuple mempty identicalTree) special
+      result = runEvalResult (asString $ eval $ Tuple mempty tree) special
+      identicalResult = runEvalResult (asString $ eval $ Tuple mempty identicalTree) special
   in result == identicalResult <?> show identicalTree
